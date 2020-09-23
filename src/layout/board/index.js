f91d4b435517b110abced2401b8a618f4eecdd05
wangjojo <weiainijiujiu@126.com>
2020-09-23 22:57:54 +0800

    react初始化

---
+++ src/layout/board/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+function init() {
+    
+}
+
+function Board() {
+    return (
+        <div className="border-container">
+            <canvas></canvas>
+        </div>
+    );
+}
+
+export default Board;
