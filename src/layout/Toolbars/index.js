510a2fd5713305f45f76e097ddccb6a552903909
wangjojo <weiainijiujiu@126.com>
2020-10-09 15:48:08 +0800

    工具栏UI，基本的图标还是初步的设计完成

--- src/layout/Toolbars/index.js
+++ src/layout/Toolbars/index.js
@@ -1,7 +1,15 @@
 import React  from 'react';
-import { useDispatch } from '../../store/index'
+import { useDispatch, useStore } from '../../store/index'
 import CONST from '../../const'
 import './index.css'
+import { ReactComponent as PenLogo } from './pen.svg'
+import { ReactComponent as RectLogo } from './rect.svg'
+import { ReactComponent as CircleLogo } from './circle.svg'
+import { ReactComponent as EraserLogo } from './eraser.svg'
+import { ReactComponent as CleanLogo } from './clean.svg'
+import { ReactComponent as UndoLogo } from './undo.svg'
+import { ReactComponent as RestoreLogo } from './restore.svg'
+
 function Toolbars() {
   const dispatch = useDispatch();
   const togglePen = (status)  => () => {
@@ -21,27 +29,49 @@ function Toolbars() {
       }
     });
   }
+  const { status } = useStore('event')
+  function btnClass (sts) {
+    return `btn ${status === sts ? 'active': ''}`
+  }
   return (
     <div className="toolbars-wrap">
       <div className="toolbars">
-        基本功能:
-        <button onClick={togglePen(CONST.PAINT)}>画笔</button>
-        <button onClick={togglePen(CONST.RECT)}>矩形框</button>
-        <button onClick={togglePen(CONST.CIRCULAR)}>圆形</button>
-        <button onClick={togglePen(CONST.ERASER)}>橡皮擦</button>
-        <button onClick={togglePen(CONST.NOONE)}>清空</button>
-        属性设置:
-        线条粗细:<input type="range" id="volume" name="volume"
-                     min="0" max="11" onChange={setSetting('lineWidth')}/>
-        颜色:<select onChange={setSetting('strokeStyle')} >
-                <option value="black">黑</option>
-                <option value="red">红</option>
-                <option value="yellow">黄</option>
-                <option value="blue">蓝</option>
-                <option value="green">绿</option>
-            </select>
-        撤回/重做: <button onClick={togglePen(CONST.UNDO)}>撤回</button>
-        <button onClick={togglePen(CONST.RESTORE)}>重做</button>
+        <div className="toolbars-area">
+          <div className={btnClass(CONST.PAINT)} onClick={togglePen(CONST.PAINT)}>
+            <PenLogo className="btn-img" />
+          </div>
+          <div className={btnClass(CONST.RECT)} onClick={togglePen(CONST.RECT)}>
+            <RectLogo className="btn-img" />
+          </div>
+          <div className={btnClass(CONST.CIRCULAR)} onClick={togglePen(CONST.CIRCULAR)}>
+            <CircleLogo className="btn-img" />
+          </div>
+          <div className={btnClass(CONST.ERASER)} onClick={togglePen(CONST.ERASER)}>
+            <EraserLogo className="btn-img" />
+          </div>
+          <div className={btnClass(CONST.NOONE)} onClick={togglePen(CONST.NOONE)}>
+            <CleanLogo className="btn-img" />
+          </div>
+        </div>
+        <div className="toolbars-area">
+          线条粗细:<input type="range" id="volume" name="volume"
+                      min="0" max="11" onChange={setSetting('lineWidth')}/>
+          颜色:<select onChange={setSetting('strokeStyle')} >
+          <option value="black">黑</option>
+          <option value="red">红</option>
+          <option value="yellow">黄</option>
+          <option value="blue">蓝</option>
+          <option value="green">绿</option>
+        </select>
+        </div>
+        <div className="toolbars-area">
+          <div className={btnClass(CONST.UNDO)} onClick={togglePen(CONST.UNDO)}>
+            <UndoLogo className="btn-img" />
+          </div>
+          <div className={btnClass(CONST.RESTORE)} onClick={togglePen(CONST.RESTORE)}>
+            <RestoreLogo className="btn-img" />
+          </div>
+        </div>
       </div>
     </div>
   );
