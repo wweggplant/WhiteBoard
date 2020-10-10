95af2feece3f1fb618f1629de2a26dc670d300e5
wangjojo <weiainijiujiu@126.com>
2020-10-10 17:30:05 +0800

    工具栏添加隐藏显示功能,修改画板图案会覆盖工具栏的问题,其实是工具栏没有加背景色,默认是透明的原因

--- src/layout/Toolbars/index.js
+++ src/layout/Toolbars/index.js
@@ -1,6 +1,6 @@
-import React  from 'react'
+import React, { useState }  from 'react'
 import ToolButton from "./ToolButton"
-import { useDispatch, useStore } from '../../store/index'
+import { useDispatch } from '../../store/index'
 import CONST from '../../const'
 import './index.css'
 import { ReactComponent as PenLogo } from './pen.svg'
@@ -32,23 +32,24 @@ function Toolbars() {
       }
     });
   }
+  const [visibility, setVisibility] = useState(true)
   return (
-    <div className="toolbars-wrap">
+    <div  className={ `toolbars-wrap ${ visibility ? 'moveDown' : 'moveUp' }` }>
       <div className="toolbars">
         <div className="toolbars-area">
           <ToolButton title="重做" status={CONST.PAINT} onClick={togglePen(CONST.PAINT)}>
             <PenLogo />
           </ToolButton>
-          <ToolButton title="重做" status={CONST.RECT} onClick={togglePen(CONST.RECT)}>
+          <ToolButton title="矩形" status={CONST.RECT} onClick={togglePen(CONST.RECT)}>
             <RectLogo />
           </ToolButton>
-          <ToolButton title="重做" status={CONST.CIRCULAR} onClick={togglePen(CONST.CIRCULAR)}>
+          <ToolButton title="圆形" status={CONST.CIRCULAR} onClick={togglePen(CONST.CIRCULAR)}>
             <CircleLogo />
           </ToolButton>
-          <ToolButton title="重做" status={CONST.ERASER} onClick={togglePen(CONST.ERASER)}>
+          <ToolButton title="橡皮檫" status={CONST.ERASER} onClick={togglePen(CONST.ERASER)}>
             <EraserLogo />
           </ToolButton>
-          <ToolButton title="重做" status={CONST.NOONE} onClick={togglePen(CONST.NOONE)}>
+          <ToolButton title="清空画板" status={CONST.NOONE} onClick={togglePen(CONST.NOONE)}>
             <CleanLogo />
           </ToolButton>
         </div>
@@ -59,7 +60,7 @@ function Toolbars() {
           <input type="color" onChange={setSetting('strokeStyle')}/>
         </div>
         <div className="toolbars-area">
-          <ToolButton title="重做" status={CONST.UNDO} onClick={togglePen(CONST.UNDO)}>
+          <ToolButton title="撤回" status={CONST.UNDO} onClick={togglePen(CONST.UNDO)}>
             <UndoLogo />
           </ToolButton>
           <ToolButton title="重做" status={CONST.RESTORE} onClick={togglePen(CONST.RESTORE)}>
@@ -72,6 +73,16 @@ function Toolbars() {
           </ToolButton>
         </div>
       </div>
+      <div className="toolbars-top-up" onClick={() => {
+        setVisibility(!visibility)
+      }} onMouseMove={ () => {
+        if(!visibility) {
+          setVisibility(true)
+        }
+      }}>
+
+      </div>
+
     </div>
   );
 }
